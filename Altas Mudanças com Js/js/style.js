9344186e834a8ecd1c95e7edac238c7d08be970b
AugustoCesar15 <augusto.costa@sptech.school>
2022-09-26 16:18:31 -0300

    Começo do DARK THEME

---
+++ Altas Mudanças com Js/js/style.js
@@ -0,0 +1,17 @@
+function mudar() {
+    var valor = 1
+
+    if (valor == 1) {
+        
+        tema.innerHTML = `Light Theme`
+
+        valor = 0
+
+    }  else if (valor == 0){
+
+        tema.innerHTML = `Dark Theme`
+
+        valor = 1
+    }
+
+}
